7d735c46f0529c7aef39174ce2fac09af871f110
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-02 01:34:59 +0000

    Fix

--- build.ps1
+++ build.ps1
@@ -1,2 +1,4 @@
 ﻿Remove-Item –path ./client –recurse
 & java -jar swagger-codegen-cli.jar generate -i https://api.cloudmersive.com/convert/docs/v1/swagger -l javascript -o client # -projectName cloudmersive-convert-api-client
+(Get-Content ./client/package.json).replace('v1', '1.0.0') | Set-Content ./client/package.json
+& npm build ./client
